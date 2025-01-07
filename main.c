9d829a3e25145695b0badaa194372aa41c1f9551
Atakan Celebi <atakancelebi2007@gmail.com>
2025-01-07 19:56:00 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    int voti[19],max=0,media=0,volte=1;
+    printf("inserisci solo voti tra 2 e 10\n");
+    for (int i=0; i<20; i++) {
+        scanf("%d",&voti[i]);
+        while (voti[i]>10 || voti[i]<2) {
+            printf("voto invalido riprova\n");
+            scanf("%d",&voti[i]);
+        }
+        if (i==0) {
+            voti[i] = max;
+        } else if (voti[i] >= max) {
+            max = voti[i];
+        }
+    }
+    for (int k=0;k<20;k++) {
+        if (voti[k] == max) {
+            volte++;
+        }
+    }
+    for (int j=0;j<20;j++) {
+        media = voti[j] + media;
+    }
+    media = media/20;
+    printf("tra i 20 voti %d e' stato il voto massimo e lo hai raggiunto %d volte con una media totale di: %d",max,volte,media);
+}
