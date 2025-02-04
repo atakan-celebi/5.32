601818c392082ed0ceba47c144e28382f4f31869
Atakan Celebi <atakancelebi2007@gmail.com>
2025-02-04 21:49:16 +0100

    aggiunto bubble sort

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+
+
+
+void bubblesort(int arr[], int grandezza){
+    int i,c=0;
+    printf("\n");
+    for (i = 1; i <= grandezza; ++i) {
+        for (int j = 0; j < grandezza - i; ++j) {
+            if (arr[j + 1] < arr[j]) {
+                int t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+                c++;
+            }
+        }
+    }
+    if (c == 0) {
+        i=grandezza;
+    }
+}
+
+
+
+
+
 int main(void) {
     int voti[19],max=0,media=0,volte=1;
     printf("inserisci solo voti tra 2 e 10\n");
@@ -24,5 +49,10 @@ int main(void) {
         media = voti[j] + media;
     }
     media = media/20;
-    printf("tra i 20 voti %d e' stato il voto massimo e lo hai raggiunto %d volte con una media totale di: %d",max,volte,media);
+    printf("hai i inseriti i seguenti voti riordinati:");
+    bubblesort(voti,19);
+    for (int i=0;i<19;i++) {
+        printf("%d ",voti[i]);
+    }
+    printf("\ntra i 20 voti %d e' stato il voto massimo e lo hai raggiunto %d volte con una media totale di: %d",max,volte,media);
 }
